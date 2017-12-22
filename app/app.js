a4bd0e9dad5b4b3e4a0a0fc3184fb930ff1f2991
Philip <philip.bedi@berkeleygroup.co.uk>
2017-12-22 12:22:39 +0000

    jade express electron app

---
+++ app/app.js
@@ -0,0 +1,144 @@
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// Environments
+var env = process.env.NODE_ENV || 'development';
+
+// Database
+var db = require('./model/db');
+// Database schema
+//var developments = require('./model/developments');
+
+var index = require(path.join(__dirname,'/routes/index'));
+//var users = require(path.join(__dirname,'/routes/users'));
+// to start the db mongod --dbpath D:\node\myelecexp\data
+var app = express();
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname+'/views/');
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/', index);
+//app.use( '/users', users );
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+/*  web server creation */
+var debug = require('debug')('bgdesktopapp:server');
+var http = require('http');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
---
+++ app/model/db.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+// to run mongod --dbpath D:\node\myelecexp\data
+var Development = new mongoose.Schema({
+  developmentid: String,
+  name: String,
+  description: String,
+  imagepath: String,
+  dateCreated: { type: Date, default: Date.now },
+  isactive: {type: Boolean , default: 1}
+});
+mongoose.model('Development', Development);
+var env = process.env.NODE_ENV || 'development';
+//console.log('db:'+env);
+var path = require('path');
+var rootPath = path.normalize(__dirname + '/../');
+var envConfig = require(rootPath+'config/env')[env];
+//console.log(envConfig);
+mongoose.connect('mongodb://'+envConfig.host+'/'+envConfig.database, function(){
+  console.log('connected to database');
+});
